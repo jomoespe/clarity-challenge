d4cdcb4c0a283e848b9416a2f8a82023c8f55572
Jose Moreno <jomoespe@gmail.com>
2019-05-23 20:25:35 +0200

    Create CreateReader to logparser

---
+++ pkg/logparser/reader.go
@@ -0,0 +1,20 @@
+package logparser
+
+import (
+	"bufio"
+	"os"
+)
+
+// CreateReader for first filename array element, or from standard input
+// if array is empty.
+func CreateReader(filenames...string) (reader *bufio.Reader, err error) {
+	if len(filenames) < 1 {
+		reader = bufio.NewReader(os.Stdin)
+	} else {
+		var file *os.File
+		if file, err = os.Open(filenames[0]); err == nil {
+			reader = bufio.NewReader(file)
+		}
+	}
+	return 
+}
