afdf20fb2d72e39e6318b4830d5000da3ba3449f
Jose Moreno <jomoespe@gmail.com>
2019-05-24 13:54:04 +0200

    Fix typos in source files license header

--- pkg/types/set_test.go
+++ pkg/types/set_test.go
@@ -1,6 +1,6 @@
 // Copyright 2018 Jomoespe. All rights reserved.
 // Use of this source code is governed by a WTFPL-style
-// license that can be found in the LICENSE file.package logparser_test
+// license that can be found in the LICENSE file.
 
 package types_test
 
